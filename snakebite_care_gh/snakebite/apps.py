9a9b855bbd6148d83dd3f1870654e02b4b2c71f5
Kofi532 <kofi532@users.noreply.github.com>
2026-08-04 10:06:45 +0000

    Rename visible branding to VenomGuard AI

--- snakebite_care_gh/snakebite/apps.py
+++ snakebite_care_gh/snakebite/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SnakebiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'snakebite'
+    verbose_name = 'VenomGuard AI'
